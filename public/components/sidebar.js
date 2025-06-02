e403be33cfe336ae39f7ff38ec50d842bd079469
bibb81 <fabrizio.cagnucci@gmail.com>
2025-06-02 22:15:25 +0200

    fix: sistema import unificato e menu hamburger - Unificato tutto su import.html - Rimosso duplicato import-file.html - Puliti file vecchi in backup - Menu hamburger già presente in import.html - TODO: aggiungere icona notifiche nell'header

--- public/components/sidebar.js
+++ public/components/sidebar.js
@@ -64,7 +64,7 @@ class Sidebar {
                             </a>
                         </li>
                         <li>
-                            <a href="/import-file.html" data-page="import">
+                            <a href="/import.html" data-page="import">
                                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                                     <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                                     <polyline points="7 10 12 15 17 10"></polyline>
